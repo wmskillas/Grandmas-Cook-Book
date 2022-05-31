064f5a82753274c0c3b6c2223e9dd57a6a910b7c
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-05-31 12:34:53 -0400

    server generated and connects. yay!

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-// placeholder
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.listen(PORT, () => {
+  console.log("Testing 123, is this on?");
+});
+
+app.get("/express_backend", (req, res) => {
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACH" });
+});
