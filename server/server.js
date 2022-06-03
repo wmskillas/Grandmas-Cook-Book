1abacb0ba274d820507b8dc1372cfd374d9d89af
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-06-03 13:36:15 -0400

    connection config and routes update

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const db = require("./config/connection");
 const routes = require("./routes");
 
