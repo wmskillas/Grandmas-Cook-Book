c11f91c9a2af967c9010311d3eb6760e2426ff24
sebastian Chunley <101592116+chuseblok@users.noreply.github.com>
2022-06-04 12:08:09 -0400

    Revert "newui"

--- client/src/components/AddRecipe.js
+++ client/src/components/AddRecipe.js
@@ -1,38 +1,38 @@
-import React, { useState } from "react";
-import ImageUploader from "react-images-upload";
+import React, { useState} from 'react';
+import ImageUploader from 'react-images-upload';
 
-function AddRecipe() {
-  const [formState] = useState({
-    title: "",
-    ingredients: "",
-    directions: "",
+
+function AddRecipe(){
+    const [formState] = useState({
+    title: '',
+    ingredients: '',
+    directions: '',
   });
-  const [errorMessage, setErrorMessage] = useState("");
-  const { title, ingredients, directions } = formState;
-
-  const [url, setImageURL] = useState(undefined);
-  const onUrlChange = (e) => {
+  const [errorMessage,setErrorMessage] = useState('');
+  const { title, ingredients, directions} = formState;
+  
+  const [url,setImageURL]=useState(undefined);
+  const onUrlChange=e=>{
     setImageURL(e.target.value);
-  };
-  const onImage = async (failedImages, successImages) => {
-    if (!url) {
-      console.log("missing URL");
-      setErrorMessage("missing a URL to upload to");
-      return;
-    }
-    try {
-      console.log("successImages", successImages);
-    } catch (err) {
-      console.log("error in upload");
-    }
-  };
+  }
+const onImage=async(failedImages, successImages)=>{
+  if(!url){
+    console.log('missing URL');
+    setErrorMessage('missing a URL to upload to');
+    return
+  }
+  try{
+console.log('successImages', successImages)
+  }catch(err){
+console.log('error in upload');
+  }
+}
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!errorMessage) {
-      console.log("Submit Form", formState);
+      console.log('Submit Form', formState);
     }
   };
-
  const UploadComponent= props =>(
   <form>
   {/* <input id='urlInput' type='text' onChange={props.onUrlChange} value={props.url}></input> */}
@@ -68,37 +68,32 @@ return (
             defaultValue={title}
            ></input>
 
-
-          <label htmlFor='ingredients'>Ingredients:</label>
-          <textarea
-            id='ingredients'
-            type='textarea'
-            name='ingredients'
-            rows='4'
+          <label htmlFor="ingredients">Ingredients:</label>
+          <textarea id="ingredients"
+            type="textarea"
+            name="ingredients"
+            rows="4"
             maxLength={500}
             defaultValue={ingredients}
-          />
+            />
 
-          <label htmlFor='directions'>Directions:</label>
-          <textarea
-            id='directions'
-            name='directions'
-            type='textarea'
+          <label htmlFor="directions">Directions:</label>
+          <textarea id="directions"
+            name="directions"
+            type="textarea"
             maxLength={500}
-            rows='5'
-            cols='5'
+            rows="5"
+            cols="5"
             defaultValue={directions}
-          />
+            />
         </div>
-
        
         
         <button className= "submit" type="submit">Submit</button>
          </form>
       </div>
       
-
     </section>
   );
-}
+  };
 export default AddRecipe;
