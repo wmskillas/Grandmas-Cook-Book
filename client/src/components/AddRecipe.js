dfcb8ae51fd52bd8a1bc4bcc9dcf1911f0c20c3a
sebastian Chunley <sebastian.chunley@gmail.com>
2022-06-04 11:50:57 -0400

    newui

--- client/src/components/AddRecipe.js
+++ client/src/components/AddRecipe.js
@@ -1,95 +1,98 @@
-import React, { useState} from 'react';
-import ImageUploader from 'react-images-upload';
+import React, { useState } from "react";
+import ImageUploader from "react-images-upload";
 
-
-function AddRecipe(){
-    const [formState] = useState({
-    title: '',
-    ingredients: '',
-    directions: '',
+function AddRecipe() {
+  const [formState] = useState({
+    title: "",
+    ingredients: "",
+    directions: "",
   });
-  const [errorMessage,setErrorMessage] = useState('');
-  const { title, ingredients, directions} = formState;
-  
-  const [url,setImageURL]=useState(undefined);
-  const onUrlChange=e=>{
+  const [errorMessage, setErrorMessage] = useState("");
+  const { title, ingredients, directions } = formState;
+
+  const [url, setImageURL] = useState(undefined);
+  const onUrlChange = (e) => {
     setImageURL(e.target.value);
-  }
-const onImage=async(failedImages, successImages)=>{
-  if(!url){
-    console.log('missing URL');
-    setErrorMessage('missing a URL to upload to');
-    return
-  }
-  try{
-console.log('successImages', successImages)
-  }catch(err){
-console.log('error in upload');
-  }
-}
+  };
+  const onImage = async (failedImages, successImages) => {
+    if (!url) {
+      console.log("missing URL");
+      setErrorMessage("missing a URL to upload to");
+      return;
+    }
+    try {
+      console.log("successImages", successImages);
+    } catch (err) {
+      console.log("error in upload");
+    }
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!errorMessage) {
-      console.log('Submit Form', formState);
+      console.log("Submit Form", formState);
     }
   };
- const UploadComponent= props =>(
-  <form>
-  {/* <input id='urlInput' type='text' onChange={props.onUrlChange} value={props.url}></input> */}
-  <ImageUploader
-  className='imageuploader'
-  key='image-uploader'
-  withIcon={true}
-  single={true}
-  withPreview={true}
-  label='Maximum size file:5MB'
-  buttonText="Choose an image"
-  onChange={props.onImage}
-  imgExtension={['.jpg', '.png', '.jpeg']}
-  maxFileSize={5242880}></ImageUploader>
-  </form>
- );
-return (  
-    <section class="recipe">
-      <form className='bg-light'id="addrecipe" onSubmit={handleSubmit}>
+  const UploadComponent = (props) => (
+    <form>
+      {/* <input id='urlInput' type='text' onChange={props.onUrlChange} value={props.url}></input> */}
+      <ImageUploader
+        className='imageuploader'
+        key='image-uploader'
+        withIcon={true}
+        single={true}
+        withPreview={true}
+        label='Maximum size file:5MB'
+        buttonText='Choose an image'
+        onChange={props.onImage}
+        imgExtension={[".jpg", ".png", ".jpeg"]}
+        maxFileSize={5242880}
+      ></ImageUploader>
+    </form>
+  );
+  return (
+    <section class='recipe'>
+      <form className='bg-light' id='addrecipe' onSubmit={handleSubmit}>
         <div>
-            <div class="mealchoicebuttons">
-                <button>Breakfast</button>
-                <button>Lunch</button>
-                <button>Dinner</button>
-            </div>
-          <label htmlFor="name">Title:</label>
-          <input
-            type="text"
-            name="title"
-            defaultValue={title}
-           ></input>
+          <div class='mealchoicebuttons'>
+            <button>Breakfast</button>
+            <button>Lunch</button>
+            <button>Dinner</button>
+          </div>
+          <label htmlFor='name'>Title:</label>
+          <input type='text' name='title' defaultValue={title}></input>
 
-          <label htmlFor="ingredients">Ingredients:</label>
-          <textarea id="ingredients"
-            type="textarea"
-            name="ingredients"
-            rows="4"
+          <label htmlFor='ingredients'>Ingredients:</label>
+          <textarea
+            id='ingredients'
+            type='textarea'
+            name='ingredients'
+            rows='4'
             maxLength={500}
             defaultValue={ingredients}
-            />
+          />
 
-          <label htmlFor="directions">Directions:</label>
-          <textarea id="directions"
-            name="directions"
-            type="textarea"
+          <label htmlFor='directions'>Directions:</label>
+          <textarea
+            id='directions'
+            name='directions'
+            type='textarea'
             maxLength={500}
-            rows="5"
-            cols="5"
+            rows='5'
+            cols='5'
             defaultValue={directions}
-            />
+          />
         </div>
-        <UploadComponent className='imageuploader' onUrlChange={onUrlChange} onImage={onImage}
-        url={url}
+        <UploadComponent
+          className='imageuploader'
+          onUrlChange={onUrlChange}
+          onImage={onImage}
+          url={url}
         ></UploadComponent>
-        <button className= "submit" type="submit">Submit</button>
+        <button className='submit' type='submit'>
+          Submit
+        </button>
       </form>
     </section>
   );
-  };
+}
 export default AddRecipe;
